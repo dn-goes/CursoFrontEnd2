d854d7d2e74a1f4f7b83a079692a121ebb158119
gimiguelf <miguelgiovana407@gmail.com>
2025-04-23 11:29:48 -0300

    23.04.2025

---
+++ POOJS/exercicio1POO.js
@@ -0,0 +1,32 @@
+class Produto {
+    constructor(nome, preco, estoque) {
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+
+    // Método para reduzir o estoque
+     vender(qtd){
+        if(this.estoque<qtd){
+            console.log("Estoque insificiente!");
+            return;
+        }
+        return this.estoque -= qtd;
+    }
+    repor(qtd){
+        return this.estoque += qtd;
+    }
+
+        //metodo exibir info
+        exibirInfo(){
+            console.log(`Produto: ${this.nome}, Preco: ${this.preco}, Estoque: ${this.estoque}`);
+        }
+    }
+    
+    let produto1 = new Produto("Arroz", 30, 100);
+    produto1.exibirInfo();
+    produto1.vender(110);
+    produto1.repor(20);
+    produto1.vender(110);
+    produto1.exibirInfo();
+    
